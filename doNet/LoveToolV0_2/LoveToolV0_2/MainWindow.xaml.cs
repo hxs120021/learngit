5a1b08654e707a73b33c799b651aac4001276499
hxs120021 <hxs120021@163.com>
2016-08-02 09:19:33 +0800

    loveToolV1.2

---
+++ doNet/LoveToolV0_2/LoveToolV0_2/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LoveToolV0_2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MediaPlayer player = new MediaPlayer();
+        DispatcherTimer timer = new DispatcherTimer();
+        Music music = new Music();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Tick += Time_tick;
+            
+        }
+
+        private void Time_tick(object sender, EventArgs e)
+        {
+            //this.NowTime.SetBinding(TextBox.TextProperty, new Binding("Value") { ElementName = "player" });
+            string s = string.Concat(DateTime.Now.ToString("mm:ss:ms"));
+            //this.NowTime.Text = music.MusicTime(s).ToString();
+            this.NowTime.Text = s;
+        }
+
+        private void SelectSong_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select Music";
+            openFileDialog.Filter = "mp3 File|*.mp3|every file|*.*";
+            openFileDialog.FileName = string.Empty;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == false)
+                return;
+            string fileName = openFileDialog.FileName;
+            this.SongName.Text = fileName;
+            player.Open(new Uri(fileName, UriKind.Relative));
+            Console.WriteLine(player.NaturalDuration);
+            this.AllTime.Text = player.NaturalDuration.ToString();
+        }
+
+        private void PlayButtoon_Click(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+            timer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+            timer.Stop();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+            timer.Stop();
+        }
+        private void BackTimeButton_Click(object sender, RoutedEventArgs e)
+        {
+            player.Position -= TimeSpan.FromSeconds(1);
+            this.NowTime.Text = music.MusicTime(player.Position).ToString();  
+        }
+        private void AdvanceTimeButton_Click(object sender, RoutedEventArgs e)
+        {
+            player.Position += TimeSpan.FromSeconds(1);
+            this.NowTime.Text = music.MusicTime(player.Position).ToString();
+        }
+        private void UsingSpeedButton_Click(object sender, RoutedEventArgs e)
+        {
+            string speed = this.SongSpeed.Text;
+            player.SpeedRatio = double.Parse(speed);
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Source source = this.Resources["source"] as Source;
+            //float s0 = float.Parse( this.NowTime.Text);
+            int s1 = DriectionCheckBox.SelectedIndex;
+            float s2 = float.Parse(this.WaitNextTime.Text);
+            int s3 = this.SelectTpye.SelectedIndex;
+            source.AddSource(new SourceBase(s1, s2, s3));
+            foreach (var item in source)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", item._comboType, item._kind, item._waitTime);
+            }
+        }
+
+        private void CreateFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            //FileStream fs = new FileStream(player);
+            string fileName = "";
+            GetMusicName(this.SongName.Text, ref fileName);
+            fileName = fileName.Substring(0, fileName.Length - 1);
+            Console.WriteLine(fileName);
+            FileStream fs = new FileStream(fileName, FileMode.Create);
+        }
+        private void GetMusicName(string MusicPath, ref string resultName)
+        {
+            Match match = Regex.Match(MusicPath, @"(^.*?\\)(.*\.)");
+            if (match.Success)
+            {
+                Console.WriteLine("{0}\n{1}",match.Groups[1].Value, match.Groups[2]);
+                resultName = match.Groups[2].Value;
+                GetMusicName(match.Groups[2].Value, ref resultName);
+            }
+        }
+        
+    }
+}
---
+++ doNet/LoveToolV0_2/LoveToolV0_2/Music.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Collections.ObjectModel;
+
+namespace LoveToolV0_2
+{
+    public enum ComboType : int
+    {
+       Short,
+       Long
+    }
+
+    public  class Music
+    {
+        public Music()
+        { }
+        public double MusicTime(TimeSpan timeSpan)
+        {
+            string time = timeSpan.ToString();
+            Match match = Regex.Match(time, @"(\d{2}):(\d{2}):(\d{2}.\d{0,7})");
+            double seconds = 0;
+            if (match.Success)
+            {
+                for(int i = 1; i < match.Groups.Count; i++)
+                {
+                    double t = double.Parse(match.Groups[i].Value);
+                    switch(i)
+                    {
+                        case 1:
+                            seconds += 3600 * t;
+                            break;
+                        case 2:
+                            seconds += 60 * t;
+                            break;
+                        case 3:
+                            seconds += t;
+                            break;
+                    }
+                }
+
+            }
+            return seconds;
+        }
+    }
+    public class SourceBase
+    {
+        public SourceBase(int kind, float waitTime, int comboType)
+        {
+            //_collederTime = collederTime;
+            _kind = kind;
+            _waitTime = waitTime;
+            _comboType = comboType;
+        }
+        //public float _collederTime { get; set; }
+        public int _kind { get; set; }
+        public float _waitTime { get; set; }
+        public int _comboType { get; set; }
+    }
+
+    public class AddComboType : ObservableCollection<ComboType>
+    {
+        public AddComboType() : base()
+        {
+            Add(ComboType.Short);
+            Add(ComboType.Long);
+        }
+    }
+    public class AddKind :ObservableCollection<int>
+    {
+        public AddKind() : base()
+        {
+            for(int i = 0; i < 7; i++)
+            {
+                Add(i);
+            }
+        }
+    }
+    public class Source : ObservableCollection<SourceBase>
+    {
+        //public List<Source> _listSource = new List<Source>();
+        public void AddSource(SourceBase sBase)
+        {
+            base.Add(sBase);
+        }
+    }
+}
